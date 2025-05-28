65f43f6a5ef882b22e74d5738c351e052b26a2c1
paologhidelli21 <paologhidelli21@gmail.com>
2025-05-28 14:41:22 +0000

    aggiornamento

--- hello.cpp
+++ hello.cpp
@@ -14,10 +14,21 @@ int main() {
     //elemento2.begin("Secondo");
     //elemento3.begin("Terzo");
 
-    Stazione elencoStazioni[3];
-    elencoStazioni[0].begin("Primo");
-    elencoStazioni[1].begin("Secondo");
-    elencoStazioni[2].begin("Terzo");
+    Stazione elencoStazioni[0xC];
+    elencoStazioni[0x0].begin("Primo");
+    elencoStazioni[0x1].begin("Secondo");
+    elencoStazioni[0x2].begin("Terzo");
+    elencoStazioni[0x3].begin("Quar");
+    elencoStazioni[0x4].begin("Quin");
+    elencoStazioni[0x5].begin("Ses");
+    elencoStazioni[0x6].begin("Sett");
+    elencoStazioni[0x7].begin("Ott");
+    elencoStazioni[0x8].begin("Non");
+    elencoStazioni[0x9].begin("Dec");
+    elencoStazioni[0xA].begin("Undi");
+    elencoStazioni[0xB].begin("Dodic");
+
+
 
     /*
     int length = sizeof(elencoStazioni) / sizeof(elencoStazioni[0]);
@@ -35,6 +46,12 @@ int main() {
     for ( Stazione el: elencoStazioni){
         cout << el.getNome() << ": " << hex << el.getId() << "\n";
     }
+    
+    int cmd;
+    cout << "Inserisci comando: ";
+    cin >> hex >> cmd;
+
+    cout << elencoStazioni[cmd].getNome() << ": " << elencoStazioni[cmd].getId() << "\n";
 
     //cout << elemento1.getNome() << ": " << hex << elemento1.getId() << "\n";
     //cout << elemento2.getNome() << ": " << hex << elemento2.getId() << "\n";
